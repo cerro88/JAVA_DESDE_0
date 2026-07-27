07aafe69eccd6847d3fb0f0aeabc7b163a73ddb6
cerro88 <you@example.com>
2026-07-27 12:09:21 +0200

    start while

---
+++ 5_while/P000_contarDel1Al5.java
@@ -0,0 +1,16 @@
+
+
+public class P000_contarDel1Al5 {
+    public static void main(String[] args) {
+       int numero = 1;
+
+        while ( numero <= 5){
+            
+            System.out.println(numero);
+
+            numero ++;
+        }
+
+        
+    }
+}
